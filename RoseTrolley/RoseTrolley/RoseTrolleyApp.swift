0b5786f5232a6e74ddbd9b249507b34a2fd7531c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-19 13:49:57 -0500

    RoseTrolley initial commit

---
+++ RoseTrolley/RoseTrolley/RoseTrolleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoseTrolleyApp.swift
+//  RoseTrolley
+//
+//  Created by Thomas Cowern on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct RoseTrolleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
