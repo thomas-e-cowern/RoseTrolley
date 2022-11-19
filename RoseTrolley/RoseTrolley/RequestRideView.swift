5b38c6e50a83eeeacc4303304240d9dac33e6716
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-19 14:01:06 -0500

    add RequestRideView

---
+++ RoseTrolley/RoseTrolley/RequestRideView.swift
@@ -0,0 +1,20 @@
+//
+//  RequestRideView.swift
+//  RoseTrolley
+//
+//  Created by Thomas Cowern on 11/19/22.
+//
+
+import SwiftUI
+
+struct RequestRideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RequestRideView_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestRideView()
+    }
+}
