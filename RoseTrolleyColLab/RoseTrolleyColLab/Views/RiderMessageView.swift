4c606a8a8d010e88ba66c25da19e273495094c3b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-20 02:47:14 -0500

    updated views

---
+++ RoseTrolleyColLab/RoseTrolleyColLab/Views/RiderMessageView.swift
@@ -0,0 +1,41 @@
+//
+//  RiderMessageView.swift
+//  RoseTrolleyColLab
+//
+//  Created by Thomas Cowern on 11/20/22.
+//
+
+import SwiftUI
+
+struct RiderMessageView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Great!")
+                    .font(.title)
+                    .foregroundColor(Color("Primary"))
+                Text("Lets View a Ride!")
+                    .font(.title)
+                    .foregroundColor(Color("Primary"))
+            }
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button("View Rides") {
+                        // Move to next screen
+                    }
+                    .buttonStyle(RoseButtonStyle())
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct RiderMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        RiderMessageView()
+    }
+}
