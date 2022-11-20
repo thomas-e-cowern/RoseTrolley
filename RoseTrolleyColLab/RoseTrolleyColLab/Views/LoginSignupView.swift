03aa0ca844bfac84cf8221bdaccea54eb905a35b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-20 02:37:53 -0500

    add LoginSignUp

---
+++ RoseTrolleyColLab/RoseTrolleyColLab/Views/LoginSignupView.swift
@@ -0,0 +1,55 @@
+//
+//  LoginSignUp.swift
+//  RoseTrolleyColLab
+//
+//  Created by Thomas Cowern on 11/20/22.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+struct LoginSignUp: View {
+    var body: some View {
+        VStack {
+            Image("rose-trolley-logo")
+            Spacer()
+                .frame(height: 150)
+            Button("Sign Up") {
+                // Sign Up
+            }
+            .buttonStyle(RoseButtonStyle())
+            SignInWithAppleButton(
+              
+            // Sign in with Apple
+                
+            onRequest: { request in
+                request.requestedScopes = [.fullName, .email]
+            },
+            onCompletion: { result in
+                switch result {
+
+                case .success (let authenticationResults):
+                  print("Authorization successful! :\(authenticationResults)")
+
+
+                case .failure(let error):
+                      print("Authorization failed: " + error.localizedDescription)
+                }
+            }
+
+          ).frame(width: 200, height: 50, alignment: .center)
+            
+            Button("Sign in wtih Google") {
+                // Sign Up
+                
+            }
+            .buttonStyle(RoseButtonStyle())
+        }
+    }
+}
+
+struct LoginSignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginSignUp()
+    }
+}
---
+++ RoseTrolleyColLab/RoseTrolleyColLab/Views/PassengersView.swift
@@ -0,0 +1,20 @@
+//
+//  PassengersView.swift
+//  RoseTrolleyColLab
+//
+//  Created by Thomas Cowern on 11/20/22.
+//
+
+import SwiftUI
+
+struct PassengersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PassengersView_Previews: PreviewProvider {
+    static var previews: some View {
+        PassengersView()
+    }
+}
