124bfc842bcd1aae5b70272ec28bcd8ad500b01f
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-20 09:17:03 -0500

    update RideMapView

--- RoseTrolleyColLab/RoseTrolleyColLab/Views/RideMapView.swift
+++ RoseTrolleyColLab/RoseTrolleyColLab/Views/RideMapView.swift
@@ -10,9 +10,10 @@ import MapKit
 
 struct RideMapView: View {
     
-    let locations = []
+    let locations: [String] = ["313 Datura street, West Palm Beach, FL 33417", "3123 Main street, West Palm Beach, FL 33417", "3149 James street, West Palm Beach, FL 33417", "391 Clematis street, West Palm Beach, FL 33417", "1202 Fern, West Palm Beach, FL 33417", "456 Beach street, West Palm Beach, FL 33417"]
     
     @State var searchText = ""
+    @State var frameHeight: CGFloat = 300
     
     @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 26.709723, longitude:  -80.064163), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
     
@@ -27,17 +28,49 @@ struct RideMapView: View {
                         HStack {
                             Image(systemName: "magnifyingglass")
                             TextField("Search ..", text: $searchText)
-                    }
+                            Button {
+                                // Book
+                            } label: {
+                                Image("rose")
+                                    .resizable()
+                                    .scaledToFit()
+                            }
+                            .frame(width: 30, height: 30)
+
+                        }
                         .foregroundColor(.gray)
                         .padding(.leading, 13)
+                        
+                        List {
+                            ForEach(searchResults, id: \.self) { location in
+                                Button {
+                                    
+                                } label: {
+                                    Text(location)
+                                        .foregroundColor(Color("Primary"))
+                                }
+
+                            }
+                        }
+                        .searchable(text: $searchText)
+                        
+                        Spacer()
                     }
                 Rectangle()
                     .foregroundColor(Color("LightGray"))
                 }
-                .frame(height: 80)
+                .frame(height: frameHeight)
                 .cornerRadius(13)
                 .padding()
             }
+        }
+    }
+    
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return locations
+        } else {
+            return locations.filter { $0.contains(searchText) }
         }
     }
 }
