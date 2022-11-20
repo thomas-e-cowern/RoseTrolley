caecbec1b9d8ba624141ab4e2fb2cd2e225ed6d6
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-20 02:42:07 -0500

    add PhoneNumberView

---
+++ RoseTrolleyColLab/RoseTrolleyColLab/Views/PhoneNumberVIew.swift
@@ -0,0 +1,48 @@
+//
+//  PhoneNumberVIew.swift
+//  RoseTrolleyColLab
+//
+//  Created by Thomas Cowern on 11/20/22.
+//
+
+import SwiftUI
+
+struct PhoneNumberVIew: View {
+    
+    @State private var phone: String = ""
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                TextField("Phone Number", text: $phone)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.bottom, 20)
+                Text("Valid Phone Number")
+                    .foregroundColor(Color("Primary"))
+                    .font(.headline)
+                    
+            }
+            .padding(.trailing, 40)
+            .padding(.leading, 40)
+            
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button("Next") {
+                        // Move to next screen
+                    }
+                    .buttonStyle(RoseButtonStyle())
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct PhoneNumberVIew_Previews: PreviewProvider {
+    static var previews: some View {
+        PhoneNumberVIew()
+    }
+}
