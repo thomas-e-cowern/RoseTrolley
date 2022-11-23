6364ea4fc8408a4a1a3688af46950a2b6af38a64
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-23 17:47:59 -0500

    UberClone initial commit

---
+++ UberClone/UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Thomas Cowern on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
