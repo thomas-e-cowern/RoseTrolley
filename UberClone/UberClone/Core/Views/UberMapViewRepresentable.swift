eefa97bbfd49da022a8d63a31a101f119b9fdd7b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-23 18:14:57 -0500

    add mapView method to UberMapViewRepresentable

--- UberClone/UberClone/Core/Views/UberMapViewRepresentable.swift
+++ UberClone/UberClone/Core/Views/UberMapViewRepresentable.swift
@@ -40,5 +40,13 @@ extension UberMapViewRepresentable {
             self.parent = parent
             super.init()
         }
+        
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
+            
+            parent.mapView.setRegion(region, animated: true)
+        }
+        
+        
     }
 }
