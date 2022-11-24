5ffc504534602f41d01dc924f2f497ff9018b56b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-23 19:13:11 -0500

    update HomeView with LocationSearchActivtionView

--- UberClone/UberClone/Core/Home/Views/HomeView.swift
+++ UberClone/UberClone/Core/Home/Views/HomeView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        UberMapViewRepresentable()
-            .ignoresSafeArea()
+        ZStack(alignment: .top) {
+            UberMapViewRepresentable()
+                .ignoresSafeArea()
+            
+            LocationSearchActivtionView()
+                .padding(.top, 72)
+        }
     }
 }
 
