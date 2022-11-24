b69725cdb5daf19a4d302fe27237dfba7983ffed
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-23 19:06:13 -0500

    add LocationSearchActivtionView

---
+++ UberClone/UberClone/Core/LocationSearch/Views/LocationSearchActivtionView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationSearchActivtionView.swift
+//  UberClone
+//
+//  Created by Thomas Cowern on 11/23/22.
+//
+
+import SwiftUI
+
+struct LocationSearchActivtionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationSearchActivtionView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchActivtionView()
+    }
+}
