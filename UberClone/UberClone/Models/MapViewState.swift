82882ca9d5bbbdefeb3b012b7eb370f9e56e08a0
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-26 13:47:11 -0500

    add MapViewState to Models folder

---
+++ UberClone/UberClone/Models/MapViewState.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewState.swift
+//  UberClone
+//
+//  Created by Thomas Cowern on 11/26/22.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case locationSelected
+}
