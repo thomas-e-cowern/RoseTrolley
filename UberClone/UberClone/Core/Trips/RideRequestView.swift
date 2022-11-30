891f83d9a9f5ddf00d9b1611823b9ff0d6c4f217
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-30 18:29:50 -0500

    add RideRequestView to Trips folder

---
+++ UberClone/UberClone/Core/Trips/RideRequestView.swift
@@ -0,0 +1,20 @@
+//
+//  RideRequestView.swift
+//  UberClone
+//
+//  Created by Thomas Cowern on 11/30/22.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RideRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        RideRequestView()
+    }
+}
