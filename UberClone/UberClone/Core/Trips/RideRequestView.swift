7ada732c52e16d39be608c963ea803e2a4429d8b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-30 18:38:07 -0500

    add capsule, location and time to RideRequestView

--- UberClone/UberClone/Core/Trips/RideRequestView.swift
+++ UberClone/UberClone/Core/Trips/RideRequestView.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct RideRequestView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Capsule()
+                .foregroundColor(Color(.systemGray))
+                .frame(width: 48, height: 6)
+            
+            // trip info view
+            HStack {
+                VStack {
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 6, height: 6)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 24)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 6, height: 6)
+                }
+                
+                VStack {
+                    HStack {
+                        Text("Current Location")
+                            .font(.system(size: 16))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("1:30 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack {
+                        Text("Starbucks")
+                            .font(.system(size: 16))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("1:45 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                }
+            }
+            
+            // ridy type selection view
+            
+            // payment option view
+            
+            // request ride button
+        }
     }
 }
 
