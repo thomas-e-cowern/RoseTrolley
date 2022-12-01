bc71dd7baf34307194e32f2652cf04291897846c
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-11-30 19:38:20 -0500

    add animation for RideRequestView

--- UberClone/UberClone/Core/LocationSearch/Views/LocationSearchView.swift
+++ UberClone/UberClone/Core/LocationSearch/Views/LocationSearchView.swift
@@ -53,8 +53,10 @@ struct LocationSearchView: View {
                     ForEach(viewModel.results, id:\.self) { result in
                         LocationSearchResultsCell(title: result.title, subtitle: result.subtitle)
                             .onTapGesture {
-                                viewModel.selectLocation(result)
-                                mapState = .locationSelected
+                                withAnimation(.spring()) {
+                                    viewModel.selectLocation(result)
+                                    mapState = .locationSelected
+                                }
                         }
                     }
                 }
