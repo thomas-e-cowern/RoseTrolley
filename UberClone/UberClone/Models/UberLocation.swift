1107868aade6976a149b88ee5f8728508f8d5cf2
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-02 17:46:07 -0500

    add UberLocation to Models folder

---
+++ UberClone/UberClone/Models/UberLocation.swift
@@ -0,0 +1,14 @@
+//
+//  UberLocation.swift
+//  UberClone
+//
+//  Created by Thomas Cowern on 12/2/22.
+//
+
+import Foundation
+import CoreLocation
+
+struct UberLocation {
+    let title: String
+    let coordinate: CLLocationCoordinate2D
+}
