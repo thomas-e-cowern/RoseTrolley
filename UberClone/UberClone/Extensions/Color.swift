3ab0dcb945d576146d0d506f61bf911ace462f9e
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-02 19:08:00 -0500

    add Color to extensions folder

---
+++ UberClone/UberClone/Extensions/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  UberClone
+//
+//  Created by Thomas Cowern on 12/2/22.
+//
+
+import Foundation
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+}
