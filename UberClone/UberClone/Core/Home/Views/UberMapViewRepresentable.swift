01b4a2eb6ee6d68d0236e818c1c983390cb48f69
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-12-02 19:03:34 -0500

    update HomeView to fix polyline error

--- UberClone/UberClone/Core/Home/Views/UberMapViewRepresentable.swift
+++ UberClone/UberClone/Core/Home/Views/UberMapViewRepresentable.swift
@@ -99,6 +99,7 @@ extension UberMapViewRepresentable {
             guard let userLocationCoordinante = self.userLocationCoordinante else { return }
             parent.locationViewModel.getDestinationRoute(from: userLocationCoordinante, to: coordinate) { route in
                 self.parent.mapView.addOverlay(route.polyline)
+                self.parent.mapState = .polylineAdded
                 let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect, edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
                 self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
             }
